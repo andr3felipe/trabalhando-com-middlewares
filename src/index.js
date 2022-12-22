ad2e6f5235486d1b9302764ff4fdb0633940df04
Andre Felipe <andre.felipe_360@hotmail.com>
2022-12-22 00:05:13 -0300

    Finished

--- src/index.js
+++ src/index.js
@@ -10,19 +10,75 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+
+  const user = users.find(user => user.username.toLowerCase() === username.toLowerCase())
+
+  if (!user) {
+    return response.status(404).json({ error: "User not found!"})
+  }
+
+  request.user = user
+
+  return next()
+
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request
+
+  const isPro = user.pro === true
+
+  if (!isPro) {
+    if (user.todos.length >= 9){
+      return response.status(403).json({ error: "User already have 10 ToDos, please upgrade your plan!"})
+    } 
+  }
+
+  return next()
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+  const { id } = request.params
+
+  const user = users.find(user => user.username.toLowerCase() === username.toLowerCase())
+
+  if (!user) {
+    return response.status(404).json({ error: "User not found!"})
+  }
+
+  const validateId = validate(id)
+
+  if (!validateId) {
+    return response.status(400).json({ error: "Not a valid uuid!"})
+  }
+
+  const findTodo = user.todos.find(todo => todo.id === id)
+
+  if (!findTodo) {
+    return response.status(404).json({ error: "This user doesn't have this ToDo ID!"})
+  }
+  
+  request.user = user
+  request.todo = findTodo
+
+  return next()
+
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params
+
+  const user = users.find(user => user.id === id)
+
+  if (!user) {
+    return response.status(404).json({ error: "User not found by this ID!"})
+  }
+
+  request.user = user
+
+  return next()
 }
 
 app.post('/users', (request, response) => {
